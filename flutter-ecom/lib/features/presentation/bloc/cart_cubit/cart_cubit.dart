aabf3c81fcc26650f6cfe5ec2b10d80446b157ad
unknown <nhd7noushadazeez@gmail.com>
2025-04-03 22:11:34 +0530

    cart items

--- flutter-ecom/lib/features/presentation/bloc/cart_cubit/cart_cubit.dart
+++ flutter-ecom/lib/features/presentation/bloc/cart_cubit/cart_cubit.dart
@@ -2,25 +2,39 @@ import 'package:bloc/bloc.dart';
 import 'package:ecom_pro/features/models/cart_item.dart';
 import 'package:flutter/material.dart';
 
+import '../../../../core/api/base_client.dart';
 import '../../../../core/api/end_points.dart';
 import '../../../../core/utils/constants.dart';
 
 part 'cart_state.dart';
 
 class CartCubit extends Cubit<CartState> {
-  CartCubit() : super(CartState());
+  CartCubit()
+      : super(CartState(
+          cartItems: [],
+        ));
+
+  final List<CartItemModel> _cartItems = [];
+
+  // add cart item
+  void addCartItem(CartItemModel cartItem) {
+    _cartItems.add(cartItem);
+
+    emit(state.copyWith(cartItems: _cartItems));
+  }
 
   // add to cart
   void addToCart({
     required BuildContext context,
+    required String userID,
   }) async {
     String url = '${EndPoints.baseUrl}/${EndPoints.cart}';
 
-    // CartModel cartModel = CartModel(userID: userID, cartItems: cartItems);
+    CartModel cartModel = CartModel(userID: userID, cartItems: _cartItems);
 
-    // logger(
-    //     '//////////// user name: ${userModel.name}, email: ${userModel.email}, phone: ${userModel.phone}, pass: ${userModel.password}, imgPath: ${userModel.image?.imagePath}, imgName: ${userModel.image?.imageName},');
+    logger(
+        '//////////// user id: ${cartModel.userID}, items: ${cartModel.cartItems},,');
 
-    // await BaseClient.createAccount(url, userModel);
+    await BaseClient.addToCart(context, url, cartModel);
   }
 }
--- flutter-ecom/lib/features/presentation/bloc/cart_cubit/cart_state.dart
+++ flutter-ecom/lib/features/presentation/bloc/cart_cubit/cart_state.dart
@@ -1,3 +1,17 @@
 part of 'cart_cubit.dart';
 
-class CartState {}
+class CartState {
+  CartState({
+    required this.cartItems,
+  });
+
+  final List<CartItemModel> cartItems;
+
+  CartState copyWith({
+    List<CartItemModel>? cartItems,
+  }){
+    return CartState(
+      cartItems: cartItems ?? this.cartItems,
+    );
+  }
+}
